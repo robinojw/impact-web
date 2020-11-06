4f417b787cea8c36026f38455777de2b58b64ac0
Robin White <robin.white@phntms.com>
2020-11-06 22:57:21 +0000

    Add bodymovin animation and integrate into scroll ani

--- src/info-section/info.js
+++ src/info-section/info.js
@@ -1,10 +1,30 @@
 import './info.css'
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactBodyMovin from 'react-bodymovin';
+import markus2 from './animation.json';
 
 class Info extends Component {
+
   render() {
+    var svgContainer = document.getElementById('svgContainer');
+    const bodyMovinOptions = {
+      loop: true,
+      autoplay: true,
+      prerender: true,
+      animationData:  markus2
+  };
     return (
-      <div className="info-section"></div>
+      <div className="info-section">
+        <div className="tracking">
+          <div className="text">
+            <h2>Impact tracks the emissions from your travel,
+              food, and products used.</h2>
+          </div>
+            <div className="animation" id="svgContainer">
+            <ReactBodyMovin options={bodyMovinOptions}/>
+            </div>
+        </div>
+      </div>
     )
   }
 }
